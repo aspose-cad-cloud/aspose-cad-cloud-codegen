0cf9d22d910f77b09374659d625bbe76cfc7190e
Evgeny Ivanov <Evgeny.Ivanov@aspose.com>
2018-01-12 15:34:34 +0700

    fix script

--- codegen/generateSDK.bat
+++ codegen/generateSDK.bat
@@ -1,7 +1,7 @@
 del /S /Q "c:\tmp\csharp" || goto :error
-java -jar SDKs\codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word.json -l csharp -t SDKs\codegen\Templates\csharp -o c:/tmp/csharp/ -c SDKs\codegen\config.json || goto :error
+java -jar codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word.json -l csharp -t codegen\Templates\csharp -o c:/tmp/csharp/ -c codegen\config.json || goto :error
 
-SDKs\codegen\Tools\SplitCSharpCodeFile.exe C:\tmp\csharp\src\Aspose.Words.Cloud.Sdk\Api\WordsApi.cs C:\tmp\csharp\src\Aspose.Words.Cloud.Sdk\Model\Requests\ || goto :error
+codegen\Tools\SplitCSharpCodeFile.exe C:\tmp\csharp\src\Aspose.Words.Cloud.Sdk\Api\WordsApi.cs C:\tmp\csharp\src\Aspose.Words.Cloud.Sdk\Model\Requests\ || goto :error
 
 del /S /Q "SDKs\NET\Aspose.Words.Cloud.Sdk\Model" || goto :error
 del /S /Q "SDKs\NET\Aspose.Words.Cloud.Sdk\Api\WordsApi.cs" || goto :error
@@ -10,9 +10,9 @@ xcopy "c:\tmp\csharp\src\Aspose.Words.Cloud.Sdk\Model" "SDKs\NET\Aspose.Words.Cl
 xcopy "c:\tmp\csharp\src\Aspose.Words.Cloud.Sdk\Api" "SDKs\NET\Aspose.Words.Cloud.Sdk\Api" /E || goto :error
 
 del /S /Q "c:\tmp\php" || goto :error
-java -jar SDKs\codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word.json -l php -t SDKs\codegen\Templates\php -o c:/tmp/php/ --invoker-package Aspose\Words --model-package Model --api-package Api || goto :error
+java -jar codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word.json -l php -t codegen\Templates\php -o c:/tmp/php/ --invoker-package Aspose\Words --model-package Model --api-package Api || goto :error
 
-SDKs\codegen\Tools\SplitPhpCodeFile.exe C:\tmp\php\SwaggerClient-php\lib\Api\WordsApi.php C:\tmp\php\SwaggerClient-php\lib\Model\Requests\ || goto :error
+codegen\Tools\SplitPhpCodeFile.exe C:\tmp\php\SwaggerClient-php\lib\Api\WordsApi.php C:\tmp\php\SwaggerClient-php\lib\Model\Requests\ || goto :error
 
 del /S /Q "SDKs\PHP\src" || goto :error
 
@@ -21,8 +21,7 @@ del /S /Q "C:\tmp\php\SwaggerClient-php\lib\Api" || goto :error
 rmdir "C:\tmp\php\SwaggerClient-php\lib\Api" || goto :error
 xcopy "C:\tmp\php\SwaggerClient-php\lib" "SDKs\PHP\src\Aspose\Words" /E || goto :error
 xcopy /y "C:\tmp\php\SwaggerClient-php\composer.json" "SDKs\PHP" || goto :error
-xcopy /y "SDKs\codegen\BuildFiles\composer.phar" "SDKs\PHP" || goto :error
-xcopy /y "SDKs\codegen\BuildFiles\RepeatRequestException.php" "SDKs\PHP\src\Aspose\Words" || goto :error
+
 
 goto :EOF
 
