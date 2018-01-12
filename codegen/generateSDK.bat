f399c3035fd2f536548138dcb760c270a350a44b
Evgeny Ivanov <Evgeny.Ivanov@aspose.com>
2018-01-12 15:29:05 +0700

    fix script

--- codegen/generateSDK.bat
+++ codegen/generateSDK.bat
@@ -1,5 +1,5 @@
 del /S /Q "c:\tmp\csharp" || goto :error
-java -jar SDKs\codegen\swagger-codegen-cli-2.3.0.jar generate -i SDKs\spec\asposeforcloud_word.json -l csharp -t SDKs\codegen\Templates\csharp -o c:/tmp/csharp/ -c SDKs\codegen\config.json || goto :error
+java -jar SDKs\codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word.json -l csharp -t SDKs\codegen\Templates\csharp -o c:/tmp/csharp/ -c SDKs\codegen\config.json || goto :error
 
 SDKs\codegen\Tools\SplitCSharpCodeFile.exe C:\tmp\csharp\src\Aspose.Words.Cloud.Sdk\Api\WordsApi.cs C:\tmp\csharp\src\Aspose.Words.Cloud.Sdk\Model\Requests\ || goto :error
 
@@ -10,7 +10,7 @@ xcopy "c:\tmp\csharp\src\Aspose.Words.Cloud.Sdk\Model" "SDKs\NET\Aspose.Words.Cl
 xcopy "c:\tmp\csharp\src\Aspose.Words.Cloud.Sdk\Api" "SDKs\NET\Aspose.Words.Cloud.Sdk\Api" /E || goto :error
 
 del /S /Q "c:\tmp\php" || goto :error
-java -jar SDKs\codegen\swagger-codegen-cli-2.3.0.jar generate -i SDKs\spec\asposeforcloud_word.json -l php -t SDKs\codegen\Templates\php -o c:/tmp/php/ --invoker-package Aspose\Words --model-package Model --api-package Api || goto :error
+java -jar SDKs\codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word.json -l php -t SDKs\codegen\Templates\php -o c:/tmp/php/ --invoker-package Aspose\Words --model-package Model --api-package Api || goto :error
 
 SDKs\codegen\Tools\SplitPhpCodeFile.exe C:\tmp\php\SwaggerClient-php\lib\Api\WordsApi.php C:\tmp\php\SwaggerClient-php\lib\Model\Requests\ || goto :error
 
