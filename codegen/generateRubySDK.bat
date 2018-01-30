0a9d9644d7e701e1f3263756f8e930e2bd9c3510
yaroslawekimov <yaroslaw.ekimov@aspose.com>
2018-01-30 08:55:38 +0700

    WORDSCLOUD-357 first step

--- codegen/generateRubySDK.bat
+++ codegen/generateRubySDK.bat
@@ -1,8 +1,11 @@
 del /S /Q "c:\tmp\ruby" || goto :error
-java -jar codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word.json -l ruby -t codegen\Templates\ruby -o c:/tmp/ruby/ -c codegen\config.json || goto :error
+java -jar codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word.json -l ruby -t codegen\Templates\ruby -o c:/tmp/ruby/ -c codegen\rubyConfig.json || goto :error
 
-del /S /Q "SDKs\RUBY\lib || goto :error
-xcopy "C:\tmp\ruby\lib\swagger_client" "SDKs\RUBY\lib" /E || goto :error
+codegen\Tools\SplitRubyCodeFile.exe C:\tmp\ruby\lib\ruby_sdk\api\words_api.rb C:\tmp\ruby\lib\ruby_sdk\models\requests\ || goto :error
+
+del /S /Q "SDKs\RUBY\lib" || goto :error
+xcopy "C:\tmp\ruby\lib\ruby_sdk" "SDKs\RUBY\lib" /E || goto :error
+xcopy "C:\tmp\ruby\lib\ruby_sdk.rb" "SDKs\RUBY\lib" || goto :error
 
 goto :EOF
 
