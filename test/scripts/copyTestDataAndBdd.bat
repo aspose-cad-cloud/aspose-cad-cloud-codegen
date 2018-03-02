cae418eb72ff43d7aed611ced7b690e8a3fe3628
Evgeny Ivanov <Evgeny.Ivanov@aspose.com>
2018-03-02 14:37:31 +0700

    added testdata and bdd requirements to repository

---
+++ test/scripts/copyTestDataAndBdd.bat
@@ -0,0 +1,7 @@
+del /S /Q "SDKs\NET\Aspose.Words.Cloud.Sdk.BddTests\Features" || goto :error
+xcopy "test\bdd\Features" "SDKs\NET\Aspose.Words.Cloud.Sdk.BddTests\Features" /E || goto :error
+xcopy "test\TestData" "SDKs\NET\TestData" /E /y || goto :error
+
+del /S /Q "SDKs\node\bdd\features" || goto :error
+xcopy "test\bdd\Features" "SDKs\node\bdd\features" /E || goto :error
+xcopy "test\TestData" "SDKs\node\TestData" /E /y || goto :error
