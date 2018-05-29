d343062c60134085723e6405fef8df8e376e3389
yaroslawekimov <yaroslaw.ekimov@aspose.com>
2018-05-29 21:31:09 +0700

    first step

---
+++ codegen/generatePythonSDK.bat
@@ -0,0 +1,8 @@
+del /S /Q "c:\tmp\python" || goto :error
+java -jar codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word.json -l python -t codegen\Templates\python -o c:/tmp/python/ || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
