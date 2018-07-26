0faddfb6dca83d53652a3284fbd47860dc1b4b5f
yaroslawekimov <yaroslaw.ekimov@aspose.com>
2018-07-26 21:33:02 +0700

    Implemented Python SDK

--- codegen/generatePythonSDK.bat
+++ codegen/generatePythonSDK.bat
@@ -1,10 +1,12 @@
 del /S /Q "c:\tmp\python" || goto :error
-java -jar codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word.json -l python -t codegen\Templates\python -c codegen\pythonConfig.json -o c:/tmp/python/ || goto :error
+java -jar codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word_python.json -l python -t codegen\Templates\python -c codegen\pythonConfig.json -o c:/tmp/python/ || goto :error
 
-codegen\Tools\SplitPythonCodeFile.exe C:\tmp\python\wordspythonsdk\apis\words_api.py C:\tmp\python\wordspythonsdk\models\requests\ || goto :error
+codegen\Tools\SplitPythonCodeFile.exe C:\tmp\python\asposewordscloud\apis\words_api.py C:\tmp\python\asposewordscloud\models\requests\ || goto :error
 
-del /S /Q "SDKs\python" || goto :error
-xcopy "C:\tmp\python" "SDKs\python" /E || goto :error
+del /S /Q "SDKs\python\docs" || goto :error
+del /S /Q "SDKs\python\asposewordscloud" || goto :error
+xcopy "C:\tmp\python\docs" "SDKs\python\docs" /E || goto :error
+xcopy "C:\tmp\python\asposewordscloud" "SDKs\python\asposewordscloud" /E || goto :error
 
 goto :EOF
 
