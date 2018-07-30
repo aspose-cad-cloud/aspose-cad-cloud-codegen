2f6458ee332c27e59ca39d19163f32dbdd6ccc1b
yaroslawekimov <yaroslaw.ekimov@aspose.com>
2018-07-30 23:23:30 +0700

    updated python sdk

--- codegen/generatePythonSDK.bat
+++ codegen/generatePythonSDK.bat
@@ -3,10 +3,10 @@ java -jar codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_
 
 codegen\Tools\SplitPythonCodeFile.exe C:\tmp\python\asposewordscloud\apis\words_api.py C:\tmp\python\asposewordscloud\models\requests\ || goto :error
 
-del /S /Q "SDKs\python\docs" || goto :error
-del /S /Q "SDKs\python\asposewordscloud" || goto :error
-xcopy "C:\tmp\python\docs" "SDKs\python\docs\" /E || goto :error
-xcopy "C:\tmp\python\asposewordscloud" "SDKs\python\asposewordscloud\" /E || goto :error
+RD /S /Q "SDKs\python\docs" || goto :error
+RD /S /Q "SDKs\python\asposewordscloud" || goto :error
+xcopy "C:\tmp\python\docs" "SDKs\python\docs" /E /I || goto :error
+xcopy "C:\tmp\python\asposewordscloud" "SDKs\python\asposewordscloud" /E /I || goto :error
 
 goto :EOF
 
