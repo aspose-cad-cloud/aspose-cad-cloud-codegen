52d029c1a05b6c072ae29c1cc4a36620af02d4fa
yaroslaw.ekimov <yaroslawekimov@aspose.com>
2019-03-06 18:31:54 +0700

    change package for java

--- codegen/generateJavaSDK.bat
+++ codegen/generateJavaSDK.bat
@@ -3,13 +3,13 @@ java -jar codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_
 
 codegen\Tools\SplitJavaCodeFile.exe C:\tmp\java\src\main\java\com\aspose\words\cloud\api\WordsApi.java C:\tmp\java\src\main\java\com\aspose\words\cloud\model\requests\ || goto :error
 
-del /S /Q "SDKs\java\src\main\java\com\aspose\words\cloud\model" || goto :error
-del /S /Q "SDKs\java\src\main\java\com\aspose\words\cloud\api\WordsApi.java" || goto :error
+del /S /Q "SDKs\java\src\main\java\com\aspose\words\model" || goto :error
+del /S /Q "SDKs\java\src\main\java\com\aspose\words\api\WordsApi.java" || goto :error
 RD /S /Q "C:\tmp\java\src\test" || goto :error
 
 
-xcopy "C:\tmp\java\src\main\java\com\aspose\words\cloud\model" "SDKs\java\src\main\java\com\aspose\words\cloud\model" /E || goto :error
-xcopy "C:\tmp\java\src\main\java\com\aspose\words\cloud\api" "SDKs\java\src\main\java\com\aspose\words\cloud\api" /E || goto :error
+xcopy "C:\tmp\java\src\main\java\com\aspose\words\cloud\model" "SDKs\java\src\main\java\com\aspose\words\model" /E || goto :error
+xcopy "C:\tmp\java\src\main\java\com\aspose\words\cloud\api" "SDKs\java\src\main\java\com\aspose\words\api" /E || goto :error
 
 goto :EOF
 
