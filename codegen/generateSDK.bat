9526297eed35beba48a8d3de8081ee25be1ecaf0
Denis Demenko <denis.demenko@aspose.com>
2020-03-09 15:38:43 +0200

    CADCLOUD-15 - fixed product naming

--- codegen/generateSDK.bat
+++ codegen/generateSDK.bat
@@ -1,16 +1,16 @@
 ::powershell -Command Invoke-WebRequest -OutFile spec\asposeforcloud_cad.json https://localhost:44350/v3.0/cad/swagger/sdkspec
-powershell -Command Invoke-WebRequest -OutFile spec\asposeforcloud_cad.json https://api-qa.aspose.cloud/v3.0/cad/swagger/sdkspec
-::powershell -Command Invoke-WebRequest -OutFile spec\asposeforcloud_cad_without_disciminator.json https://api-qa.aspose.cloud/v3.0/cad/swagger/spec
+::powershell -Command Invoke-WebRequest -OutFile spec\asposeforcloud_cad.json https://api-qa.aspose.cloud/v3.0/cad/swagger/sdkspec
+::powershell -Command Invoke-WebRequest -OutFile spec\asposeforcloud_cad_without_disciminator.json https://api-qa.aspose.cloud/v3.0/cad/swagger/sdkspec
 
-::call codegen\generateNodeJsSDK ||  goto :error
+call codegen\generateNodeJsSDK ||  goto :error
 
 call codegen\generateNetSDK ||  goto :error
 
-::call codegen\generatePhpSDK ||  goto :error
+call codegen\generatePhpSDK ||  goto :error
 
-::call codegen\generateRubySDK ||  goto :error
+call codegen\generateRubySDK ||  goto :error
 
-::call codegen\generatePythonSDK||  goto :error
+call codegen\generatePythonSDK||  goto :error
 
 call codegen\generateJavaSDK || goto :error
 
