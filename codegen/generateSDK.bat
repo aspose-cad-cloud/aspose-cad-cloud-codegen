a358e09b51dd287d23263e5aa77963f4dc79e4fe
Vitalii Kornyliuk <vitalii.kornyliuk@aspose.com>
2024-04-30 23:59:24 +0300

    line endings

--- codegen/generateSDK.bat
+++ codegen/generateSDK.bat
@@ -1,6 +1,8 @@
 ::powershell -Command Invoke-WebRequest -OutFile spec\asposeforcloud_cad.json https://localhost:44350/v3.0/cad/swagger/sdkspec
-::powershell -Command Invoke-WebRequest -OutFile spec\asposeforcloud_cad.json https://api-qa.aspose.cloud/v3.0/cad/swagger/sdkspec
-::powershell -Command Invoke-WebRequest -OutFile spec\asposeforcloud_cad_without_disciminator.json https://api-qa.aspose.cloud/v3.0/cad/swagger/sdkspec
+powershell -Command Invoke-WebRequest -OutFile ..\spec\asposeforcloud_cad.json https://api.aspose.cloud/v3.0/cad/swagger/spec
+powershell -Command Invoke-WebRequest -OutFile ..\spec\asposeforcloud_cad_without_disciminator.json https://api.aspose.cloud/v3.0/cad/swagger/sdkspec
+
+exit /b 1
 
 REM powershell -Command Invoke-WebRequest -OutFile ..\spec\asposeforcloud_cad.json https://localhost:5003/v3.0/cad/swagger/spec
 REM powershell -Command Invoke-WebRequest -OutFile ..\spec\asposeforcloud_cad_without_disciminator.json https://localhost:5003/v3.0/cad/swagger/sdkspec
