a5cc694e99b66a23d21c58403e43c042a1c732ea
Vitalii Kornyliuk <vitalii.kornyliuk@aspose.com>
2024-08-19 19:35:49 +0300

    generateSDK.sh

--- codegen/generateSDK.sh
+++ codegen/generateSDK.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PatchVersion=24.5.2
+export PatchVersion=24.5.4
 echo "Change version to $PatchVersion"
 
 #sed -i "s/__VERSION__/$PatchVersion/g" ./config.json
@@ -11,11 +11,31 @@ echo "Change version to $PatchVersion"
 
 #sed -i 's/"packageVersion": "[^"]*"/"packageVersion": '$PatchVersion'/' input.json
 #sed -i "s/\"packageVersion\": \"[^\"]*\"/\"packageVersion\": \"$PatchVersion\"/" ./config.json
-sed -i "s/\"packageVersion\": \"[^\"]*\"/\"packageVersion\": \"$PatchVersion\"/" config.json
+#sed -i "s/\"packageVersion\": \"[^\"]*\"/\"packageVersion\": \"$PatchVersion\"/" config.json
+
 #sed -i "s/packageVersion/$PatchVersion/g" ./Aspose.CAD.md
 #sed -i 's/"packageVersion": "[^"]*"/"packageVersion": "25"/' ./config.json
 #sed -i 's/"packageVersion": "[^"]*"/"packageVersion": "25"/g' ./config.json
 #sed -i 's/"packageVersion": "[^"]*"/"packageVersion": "25"/' config.json
+
+
+echo "Update Java package version"
+sed -i "s/\"artifactVersion\": \".*\"/\"artifactVersion\": \"$PatchVersion\"/" javaConfig.json
+
+echo "Update .NET package version"
+sed -i "s/\"packageVersion\": \".*\"/\"packageVersion\": \"$PatchVersion\"/" config.json
+
+echo "Update NodeJs package version"
+sed -i "s/\"npmVersion\": \".*\"/\"npmVersion\": \"$PatchVersion\"/" nodejsConfig.json
+
+echo "Update PHP package version"
+sed -i "s/\"artifactVersion\": \".*\"/\"artifactVersion\": \"$PatchVersion\"/" phpConfig.json
+
+echo "Update Python package version"
+sed -i "s/\"packageVersion\": \".*\"/\"packageVersion\": \"$PatchVersion\"/" pythonConfig.json
+
+echo "Update Ruby package version"
+sed -i "s/\"gemVersion\": \".*\"/\"gemVersion\": \"$PatchVersion\"/" rubyConfig.json
 
 echo "Build project for linux"
 
