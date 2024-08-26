725e6113bb6bf7f9147558d9c7cef4fe143ebedc
Vitalii Kornyliuk <vitalii.kornyliuk@aspose.com>
2024-08-26 17:23:49 +0300

    generateSDK.sh

--- codegen/generateSDK.sh
+++ codegen/generateSDK.sh
@@ -1,41 +1,24 @@
 #!/bin/bash
-export PatchVersion=24.5.4
-echo "Change version to $PatchVersion"
-
-#sed -i "s/__VERSION__/$PatchVersion/g" ./config.json
-#sed -i "s/__VERSION__/$PatchVersion/g" ./javaConfig.json
-#sed -i "s/__VERSION__/$PatchVersion/g" ./phpConfig.json
-#sed -i "s/__VERSION__/$PatchVersion/g" ./pythonConfig.json
-#sed -i "s/__VERSION__/$PatchVersion/g" ./rubyConfig.json
-#sed -i "s/__VERSION__/$PatchVersion/g" ./nodejsConfig.json
-
-#sed -i 's/"packageVersion": "[^"]*"/"packageVersion": '$PatchVersion'/' input.json
-#sed -i "s/\"packageVersion\": \"[^\"]*\"/\"packageVersion\": \"$PatchVersion\"/" ./config.json
-#sed -i "s/\"packageVersion\": \"[^\"]*\"/\"packageVersion\": \"$PatchVersion\"/" config.json
-
-#sed -i "s/packageVersion/$PatchVersion/g" ./Aspose.CAD.md
-#sed -i 's/"packageVersion": "[^"]*"/"packageVersion": "25"/' ./config.json
-#sed -i 's/"packageVersion": "[^"]*"/"packageVersion": "25"/g' ./config.json
-#sed -i 's/"packageVersion": "[^"]*"/"packageVersion": "25"/' config.json
-
+export PackageVersion=$ENV_PackageVersion
+echo "Change version to $PackageVersion"
 
 echo "Update Java package version"
-sed -i "s/\"artifactVersion\": \".*\"/\"artifactVersion\": \"$PatchVersion\"/" javaConfig.json
+sed -i "s/\"artifactVersion\": \".*\"/\"artifactVersion\": \"$PackageVersion\"/" javaConfig.json
 
 echo "Update .NET package version"
-sed -i "s/\"packageVersion\": \".*\"/\"packageVersion\": \"$PatchVersion\"/" config.json
+sed -i "s/\"packageVersion\": \".*\"/\"packageVersion\": \"$PackageVersion\"/" config.json
 
 echo "Update NodeJs package version"
-sed -i "s/\"npmVersion\": \".*\"/\"npmVersion\": \"$PatchVersion\"/" nodejsConfig.json
+sed -i "s/\"npmVersion\": \".*\"/\"npmVersion\": \"$PackageVersion\"/" nodejsConfig.json
 
 echo "Update PHP package version"
-sed -i "s/\"artifactVersion\": \".*\"/\"artifactVersion\": \"$PatchVersion\"/" phpConfig.json
+sed -i "s/\"artifactVersion\": \".*\"/\"artifactVersion\": \"$PackageVersion\"/" phpConfig.json
 
 echo "Update Python package version"
-sed -i "s/\"packageVersion\": \".*\"/\"packageVersion\": \"$PatchVersion\"/" pythonConfig.json
+sed -i "s/\"packageVersion\": \".*\"/\"packageVersion\": \"$PackageVersion\"/" pythonConfig.json
 
 echo "Update Ruby package version"
-sed -i "s/\"gemVersion\": \".*\"/\"gemVersion\": \"$PatchVersion\"/" rubyConfig.json
+sed -i "s/\"gemVersion\": \".*\"/\"gemVersion\": \"$PackageVersion\"/" rubyConfig.json
 
 echo "Build project for linux"
 
